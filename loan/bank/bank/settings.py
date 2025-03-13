5cbcd20224f1ddd53633080a35947cabb174ff86
taherihamed <taheri.hamed70@gmail.com>
2025-03-13 11:09:25 +0330

    add bank app

--- loan/bank/bank/settings.py
+++ loan/bank/bank/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4nk1-p8m6w&@%#_dl!jxgiywr8cn9nf%3gjrbqcl6&v)$3alaa'
+SECRET_KEY = 'django-insecure-7^(=semh+cvt*&mz9gdfneg5oez##ioudq-m$tnjld(qa1sw%&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
